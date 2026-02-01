dc64c26ef2571a5bb187e30af3977155cd9edd1f
Nazerke2909 <nazerke.kudaibergen@narxoz.kz>
2026-02-01 22:29:19 +0500

    done

--- src/main/java/edu/narxoz/galactic/Main.java
+++ src/main/java/edu/narxoz/galactic/Main.java
@@ -9,26 +9,31 @@ import edu.narxoz.galactic.task.DeliveryTask;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("=== Galactic Drone Delivery System ===\n");
         Planet earth = new Planet("Earth", 0, 0, "Nitrogen-Oxygen");
         SpaceStation iss = new SpaceStation("ISS", 100, 100, 1);
 
-        Drone light = new LightDrone("L1", 10.0);
-        Drone heavy = new HeavyDrone("H1", 100.0);
-        Cargo bifCargo = new Cargo(50.0, "Building materials");
+        Drone light = new LightDrone("L1", 10.0);  
+        Drone heavy = new HeavyDrone("H1", 100.0); 
 
+        Cargo bigCargo = new Cargo(50.0, "Building materials");
+        
         Dispatcher dispatcher = new Dispatcher();
-        DeliveryTask task = new DeliveryTask(earth, iss, bifCargo);
+        DeliveryTask task = new DeliveryTask(earth, iss, bigCargo);
 
-        Result r1= dispatcher.assigntask(task, light);
-        System.out.println("Light Drone Assignmed: " + res1.ok());
+        Result res1 = dispatcher.assignTask(task, light);
+        System.out.println("1. Assigning 50kg to LightDrone: " + (res1.ok() ? "SUCCESS" : "FAILED"));
+        System.out.println("   Reason: " + res1.reason());
 
         Result res2 = dispatcher.assignTask(task, heavy);
-        System.out.println("Heavy Drone Assignment: " + res2.ok());
-
+        System.out.println("\n2. Assigning 50kg to HeavyDrone: " + (res2.ok() ? "SUCCESS" : "FAILED"));
         if (res2.ok()) {
-            System.out.println("Estimated Time: " + task.estimateTime()); 
-            dispatcher.completeTask(task); 
-            System.out.println("Final Task State: " + task.getState());
+            System.out.println("3. Estimated Delivery Time: " + task.estimateTime() + " minutes");
+            Result res3 = dispatcher.completeTask(task);
+            System.out.println("4. Task Completion: " + (res3.ok() ? "SUCCESS" : "FAILED"));
+            System.out.println("\n--- Final Statuses ---");
+            System.out.println("Drone Status: " + heavy.getStatus()); 
+            System.out.println("Task State: " + task.getState());  
         }
     }
 }
